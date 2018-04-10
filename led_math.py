9ba7be9c188d7f6b04930922ce25c1091d69ad02
Joe McGurk <mcgurk@thelevelup.com>
2018-04-10 16:10:44 -0400

    style fixes

--- led_math.py
+++ led_math.py
@@ -4,36 +4,36 @@
 
 num_leds = 58 # 14 leds per digital, last 2 leds 2 for hyphen
 
-leds_per_segment = 14 
+leds_per_segment = 14
 
 #       LED BREAKDOWN
 #
 #           1 2
-#           - -  
+#           - -
 #       12 |   | 3
 #       11 |13 | 4
-#           - -  
+#           - -
 #       10 | 14| 5
 #       9  |   | 6
-#           - -  
+#           - -
 #           8 7
 
 #       SEGMENT BREAKDOWN
 #
 #            0
-#           - -  
+#           - -
 #        5 |   | 1
-#          | 6 | 
-#           - -  
+#          | 6 |
+#           - -
 #        4 |   | 2
-#          |   | 
-#           - -  
+#          |   |
+#           - -
 #           3
 
 led_array = [] #used to append leds at indexes
 
 for i in range(0, num_leds):
-    led_array.append(i)		
+    led_array.append(i)
 
 led_digit_0 = [] #digit X0-00
 led_digit_1 = [] #digit 0X-00
@@ -48,9 +48,9 @@ def print_pairs():
     	if i <= 13:
     	    led_digit_0.append(led_pair)
     	elif i <= (13 + (leds_per_segment*1)):
-    	    led_digit_1.append(led_pair)    
+    	    led_digit_1.append(led_pair)
     	elif i <= (13 + (leds_per_segment*2)):
-    	    led_digit_2.append(led_pair)     
+    	    led_digit_2.append(led_pair)
     	elif i <= (13 + (leds_per_segment*3)): #maxes at 55
     	    led_digit_3.append(led_pair)
     return led_digit_pairs
@@ -60,7 +60,7 @@ print_pairs() #calls print pairs - returning the array of led arrays divided by 
 current_display_numbers = [led_digit_0, led_digit_1, led_digit_2, led_digit_3]
 
 def show_number(num, led_num): #num is placeholder for which digit you want the segment from 
-    number_0 = num[0], num[1], num[2], num[3], num[4], num[5]   
+    number_0 = num[0], num[1], num[2], num[3], num[4], num[5]
     number_1 = num[0], num[1]
     number_2 = num[0], num[1], num[6], num[4], num[3]
     number_3 = num[0], num[1], num[6], num[2], num[3]
@@ -76,7 +76,7 @@ def show_number(num, led_num): #num is placeholder for which digit you want the 
 
     for i in range(0, len(led_numbers)):
 
-        if led_num == i: # if the desired number to get i.e. 1 matches i 
+        if led_num == i: # if the desired number to get i.e. 1 matches i
             getIndex = current_display_numbers.index(num) # get the digit index
             current_display_numbers[getIndex] = led_numbers[i] #assign the index those leds
             print ("hello")
@@ -85,6 +85,5 @@ def show_number(num, led_num): #num is placeholder for which digit you want the 
 
         elif led_num == number_blank:
         #    TURN LEDS OFF!!!!
-        
-              
+
 
